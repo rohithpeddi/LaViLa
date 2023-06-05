9b9d6831ce105fb05d43d476b6d7c87f7db000d8
Rohith Peddi <rxp190007@utdallas.edu>
2023-06-05 11:34:37 -0500

    Check

--- demo_narrator.py
+++ demo_narrator.py
@@ -23,18 +23,13 @@ from eval_narrator import decode_one
 
 
 def main(args):
-
-    vr = decord.VideoReader(args.video_path)
-    num_seg = 4
-    frame_ids = get_frame_ids(0, len(vr), num_segments=num_seg, jitter=False)
-    frames = video_loader_by_frames('./', args.video_path, frame_ids)
-
     ckpt_name = 'vclm_openai_timesformer_large_336px_gpt2_xl.pt_ego4d.jobid_246897.ep_0003.md5sum_443263.pth'
     ckpt_path = os.path.join('modelzoo/', ckpt_name)
     os.makedirs('modelzoo/', exist_ok=True)
     if not os.path.exists(ckpt_path):
         print('downloading model to {}'.format(ckpt_path))
-        urllib.request.urlretrieve('https://dl.fbaipublicfiles.com/lavila/checkpoints/narrator/{}'.format(ckpt_name), ckpt_path)
+        urllib.request.urlretrieve('https://dl.fbaipublicfiles.com/lavila/checkpoints/narrator/{}'.format(ckpt_name),
+                                   ckpt_path)
     ckpt = torch.load(ckpt_path, map_location='cpu')
     state_dict = OrderedDict()
     for k, v in ckpt['state_dict'].items():
@@ -46,7 +41,7 @@ def main(args):
         project_embed_dim=256,
         gated_xattn=True,
         timesformer_gated_xattn=False,
-        freeze_lm_vclm=False,      # we use model.eval() anyway
+        freeze_lm_vclm=False,  # we use model.eval() anyway
         freeze_visual_vclm=False,  # we use model.eval() anyway
         num_frames=4,
         drop_path_rate=0.
@@ -62,36 +57,52 @@ def main(args):
         Permute([3, 0, 1, 2]),
         transforms.Resize(crop_size),
         transforms.CenterCrop(crop_size),
-        transforms_video.NormalizeVideo(mean=[108.3272985, 116.7460125, 104.09373615000001], std=[68.5005327, 66.6321579, 70.32316305])
+        transforms_video.NormalizeVideo(mean=[108.3272985, 116.7460125, 104.09373615000001],
+                                        std=[68.5005327, 66.6321579, 70.32316305])
     ])
-    frames = val_transform(frames)
-    frames = frames.unsqueeze(0)  # fake a batch dimension
 
-    tokenizer = MyGPT2Tokenizer('gpt2-xl', add_bos=True)
-    with torch.no_grad():
-        if args.cuda:
-            frames = frames.cuda(non_blocking=True)
-        image_features = model.encode_image(frames)
-        generated_text_ids, ppls = model.generate(
-            image_features,
-            tokenizer,
-            target=None,  # free-form generation
-            max_text_length=77,
-            top_k=num_seg,
-            top_p=0.95,   # nucleus sampling
-            num_return_sequences=10,  # number of candidates: 10
-            temperature=0.7,
-            early_stopping=True,
-        )
+    narrations = []
+    vr = decord.VideoReader(args.video_path)
+    total_frames = len(vr)
+    num_segments = 4
+    num_return_sequences = 5
+    total_iterations = total_frames // 60
+    for iteration in range(total_iterations):
+        print('iteration {}/{}'.format(iteration, total_iterations))
+        frame_ids = get_frame_ids(iteration * 60, min((iteration + 1) * 60, total_frames), num_segments=num_segments, jitter=False)
+        frames = video_loader_by_frames('./', args.video_path, frame_ids)
 
-    for i in range(10):
-        generated_text_str = decode_one(generated_text_ids[i], tokenizer)
-        print('{}: {}'.format(i, generated_text_str))
+        frames = val_transform(frames)
+        frames = frames.unsqueeze(0)  # fake a batch dimension
+
+        tokenizer = MyGPT2Tokenizer('gpt2-xl', add_bos=True)
+        with torch.no_grad():
+            if args.cuda:
+                frames = frames.cuda(non_blocking=True)
+            image_features = model.encode_image(frames)
+            generated_text_ids, ppls = model.generate(
+                image_features,
+                tokenizer,
+                target=None,  # free-form generation
+                max_text_length=77,
+                top_k=None,
+                top_p=0.95,  # nucleus sampling
+                num_return_sequences=num_return_sequences,  # number of candidates: 10
+                temperature=0.7,
+                early_stopping=True,
+            )
+
+        print('-----------------')
+        for i in range(num_return_sequences):
+            generated_text_str = decode_one(generated_text_ids[i], tokenizer)
+            narrations.append(generated_text_str)
+            print('{}: {}'.format(i, generated_text_str))
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser('lavila narrator demo')
     parser.add_argument('--cuda', action='store_true', help='use cuda')
-    parser.add_argument('--video-path', default='/home/rxp190007/DATA/ANNOTATION/12_2/12_2_360p_spliced/output.mp4', type=str, help='video path')
+    parser.add_argument('--video-path', default='/home/rxp190007/DATA/ANNOTATION/12_2/12_2_360p.mp4', type=str,
+                        help='video path')
     args = parser.parse_args()
     main(args)
